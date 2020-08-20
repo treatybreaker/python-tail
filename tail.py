7f7e630188568a6064651a85dc03ade2a9ff9b19
San <philler3138@gmail.com>
2020-08-20 12:17:28 -0500

    Updated issues within the docstring as well as adding an example

--- tail.py
+++ tail.py
@@ -10,14 +10,18 @@ def tail(file_path, poll_rate: float = 1, start_position: int = 2) -> str:
     is less than the last known size it's likely the file was rotated so it begins to read from the start of the file
     to handle the rotation.
 
-    This is a generator so it must be accessed as though it is an iterator, e.g. with a for loop.
+    Example:
+        Tail is a generator so it must be iterated over to the get the output from whatever file that is being read::
+
+            for line in tail('somelog.log'):
+                print(line)
 
     Args:
         file_path (str): The path to the file, absolute or relative, (inclusive) to read from
         poll_rate (float): How often the file should be checked for new data (default is 1)
         start_position (int): Where to start the file with os.seek(0, start_position), 0 would be from the start of
             the file and 2 would be a normal tail.
-    Returns:
+    Yields:
         str: A single string representing any new data per line. If a previously read line has data appended to it, e.g.
             "hello" is a line and on the same line " world" is added making the line "hello world" as a whole then
             only the new data on that line " world" will be returned.
